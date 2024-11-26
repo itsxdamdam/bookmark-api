1a5c1ef122877efe91c7b22e1fb5b884f8f29b54
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-27 00:23:51 +0100

    update app module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],   
+  imports: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { message: 'I am signup' };
+  }
+
+  signin() {
+    return { message: 'I am signin' };
+  }
+}
