8e07afb201375ac15a50b003d76099fb85e5d53e
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-27 20:44:40 +0100

    feat: signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
-import { Body, Controller, Post} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -8,10 +12,7 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({
-      dto,
-    })
-    return this.authService.signup();
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: ""
+          url: "postgresql://postgres:2345@localhost:5434/nest?schema=public"
         }
       }
     })
